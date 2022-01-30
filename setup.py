08f5c138410158e556ae751386848667d9ebd910
Marcus Mann <mpm7cf@virginia.edu>
2022-01-30 13:01:03 -0500

    Init repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='apma_3100_projects',
+    version='',
+    packages=[''],
+    url='https://github.com/the-mann',
+    license='',
+    author='the-mann',
+    author_email='marcuspmann@gmail.com',
+    description='Contains the simple APMA 3100 (Probability) projects'
+)
