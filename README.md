1ed69c29f0fc4b71c4ccd3dd2bbdd6322e8406b1
Marcus Mann <mpm7cf@virginia.edu>
2022-01-30 13:05:00 -0500

    stub out README, project 1

---
+++ README.md
@@ -0,0 +1,7 @@
+# APMA Projects
+
+
+
+## Authors:
+- [Teddy Walsh](https://github.com/teddy-walsh)
+- [Marcus Mann](https://github.com/the-mann)
---
+++ project_1/main.py
@@ -0,0 +1,10 @@
+# Problem 1. TODO: @teddy-walsh
+
+
+# Problem 2. TODO: @teddy-walsh
+
+
+# Problem 3. TODO: @the-mann
+
+
+# Problem 4. TODO: @the-mann
