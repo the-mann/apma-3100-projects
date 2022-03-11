a787591a40281f534a9dde678c1a689b528af689
Marcus Mann <mpm7cf@virginia.edu>
2022-03-10 23:19:28 -0500

    convert random numbers to generator instead of recursion, added test for 4 times through

--- project_2/main.py
+++ project_2/main.py
@@ -1,5 +1,5 @@
-import decimal
 import math
+import sys
 
 X_I = 1000
 a = 24693
@@ -10,21 +10,20 @@ K = pow(2, 17)
 
 L = 1 / 12
 
+sys.setrecursionlimit(100000)
+
 
 class OutOfDomainError(ArithmeticError):
     pass
 
 
-def generate_random_number(i) -> decimal:
-    if i == 0:
-        ret = X_I
-    else:
-        ret = (generate_random_number(i - 1) * a + c)
-    return ret % K
-
-
-def rand_num(i):
-    return generate_random_number(i) / K
+def rand_num_generator(num_of_random_numbers):
+    i = 0
+    prev = X_I % K
+    while i < num_of_random_numbers:
+        prev = (prev * a + c) % K
+        i += 1
+        yield prev / K
 
 
 def cdf(x_i):
@@ -34,9 +33,9 @@ def cdf(x_i):
     :param x_i: The continuous variable
     :return: u_i, the probability that X ≤ x_i
     """
-    if (x_i < 0):
+    if x_i < 0:
         raise OutOfDomainError(f"x_i must be ≥ 0. Instead, it was {x_i}")
-    u_i = L * pow(math.e, -L * x_i)
+    u_i = 1 - pow(math.e, -L * x_i)
     return u_i
 
 
@@ -49,5 +48,31 @@ def cdf_inv(u_i):
     """
     if u_i < 0 or u_i > 1:
         raise OutOfDomainError(f"u_i must be inclusively between 0 and 1. Instead, it was {u_i}")
-    x_i = (math.log(u_i / L, math.e)) / -L
+
+    x_i = (math.log(1 - u_i, math.e)) / -L
     return x_i
+
+
+def guess_time_till_pickup(u_i):
+    """
+    No matter what, it will take her 6 seconds at least.
+    If x_i1 > .2, customer is not busy. otherwise, customer is busy and hang up immediately
+    :param u_i: The random number between 0 and 1 for checking if customer is busy or unavailable
+    """
+    # Initial time to start up phone
+    time_till_pickup = 6
+
+    if u_i < 0.2:
+        return time_till_pickup + 3, False
+    elif u_i < 0.3:
+        return time_till_pickup + 25 + 1, False
+    else:
+        # If neither of these variables are true, check what the expected time is for supplied probability
+
+        # Note that we will only wait for 25 seconds. If it takes longer than that, we'll hang up
+        if cdf_inv(u_i) > 25:
+            return time_till_pickup + 25 + 1, False
+        else:
+            return time_till_pickup + cdf_inv(u_i), True
+
+
--- project_2/test_main.py
+++ project_2/test_main.py
@@ -1,23 +1,45 @@
+import statistics
 from unittest import TestCase
 
-from project_2.main import rand_num, cdf, cdf_inv, OutOfDomainError
+from project_2.main import rand_num_generator, cdf, cdf_inv, OutOfDomainError, guess_time_till_pickup
 
 
 class Test(TestCase):
     def test_first_three_numbers(self):
-        self.assertAlmostEqual(rand_num(1), 0.4195, 4)
-        self.assertAlmostEqual(rand_num(2), 0.0425, 4)
-        self.assertAlmostEqual(rand_num(3), 0.1274, 4)
+        self.assertListEqual([round(i, 4) for i in rand_num_generator(3)], [0.4195, 0.0425, 0.1274])
 
     def test_51_52_53(self):
         self.skipTest("not implemented")
 
     def test_cdf_inverse_is_inverse(self):
-        self.assertAlmostEqual(cdf(50), 0.0012919877999174442)
-        self.assertAlmostEqual(cdf_inv(0.0012919877999174442), 50)
+        self.assertAlmostEqual(cdf(50), 0.9844961464009907)
+        self.assertAlmostEqual(cdf_inv(0.9844961464009907), 50)
 
     def test_cdf_less_than_zero(self):
         with self.assertRaises(OutOfDomainError) as cm:
             cdf(-1)
         with self.assertRaises(OutOfDomainError) as cm:
             cdf_inv(1.0000001)
+
+    def test_average_of_guess_time_till_pickup(self):
+        x = [guess_time_till_pickup(cdf(n))[0] for n in rand_num_generator(10000)]
+        self.assertAlmostEqual(9, statistics.mean(x))
+
+    def test_million_person_4_times(self):
+        rand_numbers = [x for x in rand_num_generator(10000000)]
+        z = []
+        for j in range(1000000):
+            x = 0
+            for i in range(4):
+                g = guess_time_till_pickup(rand_numbers[4 * j + i])
+                x += g[0] if g[1] else 0
+            z.append(x)
+        self.assertAlmostEqual(41, statistics.fmean(z), 1)
+
+    def test_average_random_number_2_decimal_places(self):
+        x = [x_i for x_i in rand_num_generator(100000)]
+        self.assertAlmostEqual(.5, statistics.mean(x), 2)
+        pass
+
+    def test_sum_of_cdf(self):
+        self.assertEqual(1, cdf(100000))
